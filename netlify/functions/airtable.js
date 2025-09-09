ed71ae9eb1306eb77e33ee01f8ff89292331ba88
Evestar-MaksymBiljachenko <max.biljachenko@evestar.com>
2025-09-09 17:17:41 +0300

    update

---
+++ netlify/functions/airtable.js
@@ -0,0 +1,11 @@
+export async function handler(event) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify({ message: "Hello from Airtable proxy!" }),
+  };
+}
